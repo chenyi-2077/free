16387f56471830afcd300f274c89739b02aa608d
chenyi-2077 <chenyi-2077@users.noreply.github.com>
2026-07-03 17:53:29 +0800

    fix: update DB password to @Aa20185476 in freelite/DBUtil.java

--- freelite/src/com/freelite/util/DBUtil.java
+++ freelite/src/com/freelite/util/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/freelite?useSSL=false&serverTimezone=Asia/Shanghai&characterEncoding=utf8&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASSWORD = "***";
+    private static final String PASSWORD = "@Aa20185476";
 
     static {
         try {
